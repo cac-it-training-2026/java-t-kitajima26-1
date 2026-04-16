cc6044d81c8f48c6e82933149d50492855df3d86
t-kitajima <t-kitajima@CD26305L.arksystems.co.jp>
2026-04-16 13:47:51 +0900

    Question7が完了

--- java_comprehension_exercises_volume1/src/advanced/question07/Question07_1.java
+++ java_comprehension_exercises_volume1/src/advanced/question07/Question07_1.java
@@ -1,13 +1,36 @@
 package advanced.question07;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question07_1 {
 
 	public static void main(String[] args) throws IOException {
 
 		// TODO:ここに実装
+		String[] itemNames = new String[5];
 
+		int[] prices = new int[5];
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		for (int i = 0; i < itemNames.length; i++) {
+			System.out.print("商品" + (i + 1) + "を入力してください:>");
+			itemNames[i] = br.readLine();
+		}
+		System.out.println("各商品の単価を順に入力してください：");
+
+		for (int i = 0; i < prices.length; i++) {
+			System.out.print(itemNames[i] + "の単価:>");
+			prices[i] = Integer.parseInt(br.readLine());
+		}
+
+		System.out.println("登録商品一覧");
+
+		for (int i = 0; i < itemNames.length; i++) {
+			System.out.println(itemNames[i] + "：" + prices[i] + "円");
+		}
 	}
 
 }
--- java_comprehension_exercises_volume1/src/advanced/question07/Question07_3.java
+++ java_comprehension_exercises_volume1/src/advanced/question07/Question07_3.java
@@ -1,6 +1,8 @@
 package advanced.question07;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question07_3 {
 
@@ -15,6 +17,35 @@ public class Question07_3 {
 
 		// TODO: 実装ここから
 
+		int[] stocks = new int[5];
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		for (int i = 0; i < itemNames.length; i++) {
+			System.out.print(itemNames[i] + "の在庫数:>");
+			stocks[i] = Integer.parseInt(br.readLine());
+		}
+		int buyNum;
+		for (int i = 0; i < itemNames.length; i++) {
+			System.out.print(itemNames[i] + "の購入数:>");
+			buyNum = Integer.parseInt(br.readLine());
+			if (buyNum <= stocks[i]) {
+				amounts[i] = buyNum;
+				stocks[i] -= buyNum;
+			} else {
+				System.out.println("在庫を超えています。もう一度入力してください。");
+				i--;
+			}
+		}
+
+		for (int i = 0; i < itemNames.length; i++) {
+			System.out.println(itemNames[i] + "：購入" + amounts[i] + "個\n在庫" + stocks[i] + "個");
+		}
+		int total = 0;
+		for (int i = 0; i < itemNames.length; i++) {
+			total += amounts[i] * prices[i];
+		}
+		System.out.println("合計金額" + total + "円");
 
 	}
 }
